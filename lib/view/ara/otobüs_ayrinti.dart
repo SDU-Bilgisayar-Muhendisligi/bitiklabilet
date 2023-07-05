691bfa9d8e14c0d9e57cea584204034ef64066a8
nurayarac <nurayarac19@gmail.com>
2023-07-06 02:12:18 +0300

    n

--- lib/view/ara/otobüs_ayrinti.dart
+++ lib/view/ara/otobüs_ayrinti.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Otobus_Detay extends StatefulWidget{
-  final String selectedNereden;
 
- Otobus_Detay({Key? key, required this.selectedNereden}) : super(key:key);
+
+ Otobus_Detay({Key? key}) : super(key:key);
 
   @override
   _Otobus_DetayState createState() => _Otobus_DetayState();
@@ -13,74 +13,6 @@ class Otobus_Detay extends StatefulWidget{
 }
 
 class _Otobus_DetayState extends State<Otobus_Detay> {
-  late String _selectedNereden;
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedNereden = widget.selectedNereden;
-  }
-
-
-  List<String> busCompanies = [
-    "Ayar Aydın Seyahat",
-    "Anadolu Ulaşım Turizm",
-    "Balıkesir Haksar Turizm",
-    "Buzlu Turizm",
-    "İstanbul Kalesi",
-    "Kamil Koç",
-    "Kale Seyahat",
-    "Metro Turizm",
-    "Pamukkale Turizm",
-    "Villa Seyahat",
-    "Varan Turizm"
-  ];
-  List<String> _selectedBusCompanies= [];
-
-  bool _selectAll = false;
-
-  void _toggleSelectAll(){
-    setState(() {
-      if(_selectAll){
-        _selectedBusCompanies.clear();
-      } else{
-        _selectedBusCompanies = List.from(busCompanies);
-      }
-      _selectAll = !_selectAll;
-    });
-  }
-
-  void _toggleCompany(String company){
-    setState(() {
-      if(_selectedBusCompanies.contains(company)){
-        _selectedBusCompanies.remove(company);
-      } else{
-        _selectedBusCompanies.add(company);
-      }
-      _selectAll=false;
-    });
-  }
-  List<String> _selectedTimeSlots = [];
-
-  void _toggleTimeSlot(String timeSlot) {
-    if (_selectedTimeSlots.contains(timeSlot)) {
-      setState(() {
-        _selectedTimeSlots.remove(timeSlot);
-      });
-    } else {
-      setState(() {
-        _selectedTimeSlots.add(timeSlot);
-      });
-    }
-  }
-
-
-  List<String> timeSlots = [
-    "Sabaha Karsi (00.00 - 05.00)",
-    "Sabah (05.00 - 12.00 arasi)",
-    "Oglen (12.00 - 17.00 arasi)",
-    "Gece (22.00 - sonraki gun 05.00 arasi)"
-  ];
   @override
   Widget build(BuildContext context) {
 
@@ -91,110 +23,6 @@ class _Otobus_DetayState extends State<Otobus_Detay> {
     ),
       body:Column(
         children: [
-           ElevatedButton(
-             child: Text('FİLTRE'),
-             style: ElevatedButton.styleFrom(
-          primary: Colors.red,
-        ),
-              onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (BuildContext context) {
-                return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        color:Colors.red,
-                        child: Text("FİLTRE",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),),
-                      ),
-                      Expanded(
-                        child: DefaultTabController(
-                            length: 2,
-                            child: Column(
-                              children: [
-                                TabBar(
-                                  tabs: [
-                                    Tab(child:
-                                    Text('Firmaya Göre',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                      ),),
-                                    ),
-                                    Tab(child:
-                                    Text('Saate Göre',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                    ),),
-                                    ),
-                                  ],
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          TextButton(
-                                            onPressed: () => _toggleSelectAll(),
-                                            child: Text(_selectAll ? 'Seçilenleri Kaldır' : 'Tümünü Seç'),
-                                          ),
-                                          TextButton(
-                                            onPressed: () => setState(() => _selectedBusCompanies.clear()),
-                                            child: Text('Filtrele'),
-                                          ),
-                                        ],
-                                      ),
-                                      Expanded(
-                                        child: ListView.builder(
-                                          itemCount: busCompanies.length,
-                                          itemBuilder: (BuildContext context, int index) {
-                                            return CheckboxListTile(
-                                              title: Text(busCompanies[index]),
-                                              value: _selectedBusCompanies.contains(busCompanies[index]),
-                                              onChanged:(bool? value) {
-                                                if (value != null) {
-                                                  _toggleCompany(busCompanies[index]);
-                                                }
-                                              },
-                                            );
-                                          },
-                                        ),
-                                      ),
-                                      Expanded(
-                                        child: ListView.builder(
-                                          itemCount: timeSlots.length,
-                                          itemBuilder: (BuildContext context, int index) {
-                                            return CheckboxListTile(
-                                              title: Text(timeSlots[index]),
-                                              value: _selectedTimeSlots.contains(timeSlots[index]),
-                                              onChanged:(bool? value) {
-                                                if (value != null) {
-                                                  _toggleTimeSlot(timeSlots[index]);
-                                                }
-                                              },
-                                            );
-                                          },
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-
-                              ],
-                            ),
-                        ),
-                      ),
-
-                    ],
-                );
-              },
-          );
-        },
-      ),
           Divider(),
           Expanded(
             child: SingleChildScrollView(
@@ -226,7 +54,7 @@ class _Otobus_DetayState extends State<Otobus_Detay> {
                                 Padding(
                                     padding: const EdgeInsets
                                         .symmetric(vertical: 4)),
-                                Text(_selectedNereden,
+                                Text("eddgdghf",
                                   style: TextStyle(
                                     fontSize: 16,
                                     color: Colors.pink,
@@ -322,7 +150,7 @@ class _Otobus_DetayState extends State<Otobus_Detay> {
                               padding: const EdgeInsets
                                   .symmetric(
                                   vertical: 4)),
-                          Text(_selectedNereden,
+                          Text("isdfssfb",
                             style: TextStyle(
                               fontSize: 16,
                             ),
--- lib/view/hesabım/oturum/hb_görüntüleme1.dart
+++ lib/view/hesabım/oturum/hb_görüntüleme1.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HesapGoruntuleme1 extends StatefulWidget {
@@ -7,6 +8,29 @@ class HesapGoruntuleme1 extends StatefulWidget {
   _HesapGoruntuleme1State createState() => _HesapGoruntuleme1State();
 }
 class _HesapGoruntuleme1State extends State<HesapGoruntuleme1>{
+  String _email = "";
+  String _nameSurname = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+
+  void getData() async {
+    final firestoreInstance = FirebaseFirestore.instance;
+    firestoreInstance.collection("users").doc("user_id").get().then((value) {
+      if (value.exists) {
+        String email = value.data()!["email"];
+        String nameSurname = value.data()!["nameSurname"];
+        setState(() {
+          _email= email;
+          _nameSurname= nameSurname;
+        });
+      }
+    });
+  }
   @override
   Widget build(BuildContext context){
     return MaterialApp(
@@ -21,14 +45,39 @@ class _HesapGoruntuleme1State extends State<HesapGoruntuleme1>{
           ),
           title: Text('Hesabım'),
         ),
-        body: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.black12,
-              width: 2,
-            ),
-          ),
-        ),
+        body: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20),
+                Text("Kişisel Bilgiler",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Divider(thickness: 1),
+                SizedBox(height: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("E-MAIL",style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(_email),
+                    SizedBox(height: 10),
+                    Divider(thickness: 1),
+                    SizedBox(height: 20),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("AD SOYAD",style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(_nameSurname),
+                    SizedBox(height: 10),
+                    Divider(thickness: 1),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ],
+
+        )
+
       ),
     );
   }
